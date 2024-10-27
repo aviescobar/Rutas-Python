dbcbaf873135b56c2039adf58739ca6ef9b2eb82
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-10-26 22:00:45 -0600

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -11,3 +11,5 @@ def obtener_ruta(partida, destino, paradas, api_key):
 
     response = requests.get(url)
     data = response.json()
+
+    if data["status"] == "OK" and "routes" in data and len(data["routes"]) > 0:
