780e1b8e4958dcc7b1fd74693ad7312bcff4d542
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-10-26 21:59:09 -0600

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -5,3 +5,6 @@ def obtener_ruta(partida, destino, paradas, api_key):
   origin = partida.replace(" ", "+")
   destination = destino.replace(" ", "+")
   waypoints = "|".join(paradas.split(","))
+
+   #Acceso a google Maps
+    url = f"https://maps.googleapis.com/maps/api/directions/json?origin={origin}&destination={destination}&waypoints={waypoints}&key={api_key}"
