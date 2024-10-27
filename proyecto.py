52afedadb2f6a73cf7d0c8b12323e91bab39915a
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-10-26 22:11:24 -0600

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -34,3 +34,4 @@ def obtener_coordenadas(data):
 
   # Agregar la coordenada del destino final
     lat, lng = data["routes"][0]["legs"][0]["steps"][-1]["end_location"]["lat"], data["routes"][0]["legs"][0]["steps"][-1]["end_location"]["lng"]
+    coordenadas.append((lat, lng))
