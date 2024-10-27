2f13b26416b3985607a3f244fc3042b560f4dced
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-10-26 22:14:14 -0600

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -39,6 +39,10 @@ def obtener_coordenadas(data):
 
 
 def calcular_costo_aproximado(distancia, costo_combustible_litro, consumo_litro, costo_peaje, gastos_adicionales): 
+    costo_combustible = (distancia / consumo_litro) * costo_combustible_litro
+    costo_total = costo_combustible + costo_peaje + gastos_adicionales
+    return costo_total
+
 
     
 
